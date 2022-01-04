8a7cd67acb522f811590d6b7c4c5e7668ace8a07
Maksim <85397052+GaiusCeasar@users.noreply.github.com>
2022-01-05 00:18:40 +0300

    add home work №1

---
+++ lesson_1/lesson_1.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang=ru>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson №1</title>
+</head>
+<body>
+
+    <h1>Ответы на урок №1</h1>
+    <p>
+    1. Работа сервера настроена <br>
+    2. Закладка с manual добавлена <br>
+    3.
+    <?php
+    var_dump(3 / 1);
+    var_dump(1 / 3);
+    var_dump('20cats' + 40);
+    var_dump(18 % 4);
+    ?> <br>
+    4. <br>
+        4.1. - 2 <br>
+        4.2. - 4 <br>
+    5.
+    <?php
+    var_dump(1 == 1.0);
+    var_dump(1 === 1.0);
+    var_dump('02' == 2);
+    var_dump('02' === 2);
+    var_dump('02' == '2');
+    ?> <br>
+    6.
+    <?php
+    $x = true xor true;
+    var_dump($x);
+    ?> <br>
+    Результат выполнения операции истинен тогда, когда один из аргументов истинен, а другой — ложен
+    </p>
+</body>
+</html>
